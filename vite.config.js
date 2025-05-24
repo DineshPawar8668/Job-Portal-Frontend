bcf5e459984a96916a2882bf7db5fbed974a52f3
Dinesh Pawar <131751260+DineshPawar8668@users.noreply.github.com>
2025-05-24 09:09:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc'; // ✅ you are using this
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   base: '/',
