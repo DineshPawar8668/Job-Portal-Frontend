328f7a940c7538cab3611c791618e000a06ac24e
Dinesh Pawar <131751260+DineshPawar8668@users.noreply.github.com>
2025-05-24 20:32:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// export default defineConfig({
+//   base: '/',
+//   plugins: [react()],
+// });
+
+
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
+base: '/job-portal-frontend/',
+plugins: [react()],
 });
